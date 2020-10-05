6b4aabb5eab4777c47c3f92c6635ca0e0415eb49
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2020-10-06 00:05:48 +0900

    内装-vue2020/10/06

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,46 @@
 <template>
   <div id="app">
-    <header class="ama">
-      <div class="haya">
-        <p>ID創社</p>
-      </div>
+    <nav class="ama">
+      <router-link to="/" tag="p">ID創社</router-link>
       <ul class="menu">
-        <li>会社概要</li>
-        <li>施工実績</li>
-        <li>採用</li>
-        <li>ログイン/登録</li>
-        <li>ログアウト</li>
+        <router-link to="/kaisya" tag="p">会社概要</router-link>
+        <router-link to="/" tag="p">施工実績</router-link>
+        <router-link to="/" tag="p">採用</router-link>
+        <router-link to="/" tag="p">ログイン/登録</router-link>
+        <router-link to="/" tag="p">ログアウト</router-link>
       </ul>
-    </header>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import "normalize.css";
-
 export default {
   name: "App",
 };
 </script>
 
 <style scoped>
+ul {
+  padding: 0;
+  height: 75px;
+}
 .ama {
-  height: 100px;
-  display: flex;
   display: table;
   width: 100%;
-  margin: 25px 0 25px 0;
-}
-.haya {
-  display: table-cell;
-  vertical-align: middle;
-  text-align: center;
+  margin: 15px 0 15px 0;
 }
 .menu {
   list-style-type: none;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  margin-left: 60px;
+  margin: 0 60px;
 }
 #app {
   font-family: "M PLUS Rounded 1c";
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   font-size: 30px;
   text-align: center;
   color: pink;
